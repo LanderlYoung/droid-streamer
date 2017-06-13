d582e8d9d940fc98ca74310ad7b0672c52c0482b
LanderlYoung <landerlyoung@gmail.com>
2017-06-14 01:08:15 +0800

    add permission

--- app/src/main/java/io/github/landerlyoung/droidstreamer/MainActivity.kt
+++ app/src/main/java/io/github/landerlyoung/droidstreamer/MainActivity.kt
@@ -3,7 +3,6 @@ package io.github.landerlyoung.droidstreamer
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import java.util.stream.Stream
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +12,6 @@ class MainActivity : Activity() {
         findViewById(R.id.stream_button)?.setOnClickListener {
             val intent = Intent(this, StreamingService::class.java)
             startService(intent)
-            StreamingService.GET_STREAMING_UTL
         }
     }
 }
