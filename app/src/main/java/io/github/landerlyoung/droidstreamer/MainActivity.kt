f745b89900b5e2dd244e8f4bde85f6eafc34ac08
LanderlYoung <landerlyoung@gmail.com>
2017-06-14 00:43:49 +0800

    add more code

--- app/src/main/java/io/github/landerlyoung/droidstreamer/MainActivity.kt
+++ app/src/main/java/io/github/landerlyoung/droidstreamer/MainActivity.kt
@@ -1,11 +1,19 @@
 package io.github.landerlyoung.droidstreamer
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import java.util.stream.Stream
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById(R.id.stream_button)?.setOnClickListener {
+            val intent = Intent(this, StreamingService::class.java)
+            startService(intent)
+            StreamingService.GET_STREAMING_UTL
+        }
     }
 }
--- app/src/main/java/io/github/landerlyoung/droidstreamer/StreamingService.kt
+++ app/src/main/java/io/github/landerlyoung/droidstreamer/StreamingService.kt
@@ -2,7 +2,10 @@ package io.github.landerlyoung.droidstreamer
 
 import android.app.Service
 import android.content.Intent
-import android.os.IBinder
+import android.os.Handler
+import android.os.Messenger
+import android.os.RemoteException
+import android.util.Log
 
 /**
  * <pre>
@@ -14,9 +17,39 @@ import android.os.IBinder
  */
 
 class StreamingService : Service() {
-    override fun onBind(intent: Intent?): IBinder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    lateinit var messenger: Messenger
+
+    companion object X{
+        const val TAG = "StreamingService"
+        const val MSG_GET_STREAMING_UTL = 0
+        const val MSG_GET_CURRENT_STATUS = 1
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        messenger = Messenger(Handler { msg ->
+            if (msg.replyTo != null) {
+                try {
+                    when (msg.what) {
+                        MSG_GET_STREAMING_UTL -> {
+                        }
+                        MSG_GET_CURRENT_STATUS -> {
+                        }
+                        else -> return@Handler false
+                    }
+                    return@Handler true
+                } catch (e: RemoteException) {
+                    Log.e(TAG, "error in handle message", e)
+                }
+            }
+            return@Handler false
+        })
+    }
+
+    override fun onBind(intent: Intent?) = messenger.binder!!
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return START_NOT_STICKY
+    }
 
 }
