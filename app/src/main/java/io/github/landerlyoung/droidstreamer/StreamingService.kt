b56ae527517aa048c84ff7bef965fdb2bf67051c
LanderlYoung <landerlyoung@gmail.com>
2017-06-13 23:48:43 +0800

    clean up support lib

---
+++ app/src/main/java/io/github/landerlyoung/droidstreamer/StreamingService.kt
@@ -0,0 +1,22 @@
+package io.github.landerlyoung.droidstreamer
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+/**
+ * <pre>
+ * Author: taylorcyang@tencent.com
+ * Date:   2017-06-13
+ * Time:   23:23
+ * Life with Passion, Code with Creativity.
+ * </pre>
+ */
+
+class StreamingService : Service() {
+    override fun onBind(intent: Intent?): IBinder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
